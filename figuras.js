274641540fe9f23fe0a62c76ec4f7ca1ace25df9
J-eris <erickxt@hotmail.com>
2023-03-27 18:41:22 -0600

    Integrando cálculos de cuadrado y triangulo

--- figuras.js
+++ figuras.js
@@ -1,5 +1,22 @@
 
+//Código del cuadrado
+console.group("Cuadrados");
+//const ladoCuadrado = 5;
+//console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
 
+function perimetroCuadrado(lado) {
+	return lado * 4;
+} 
+//console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+function areaCuadrado(lado) {
+	return lado * lado;
+} 
+//console.log("El area del cuadrado es: " + areaCuadrado + "cm^2");
+
+console.groupEnd();
+
+//Codigo Triangulo
 function perimetroTriangulo(lado1, lado2, lado3) {
   	return lado1 + lado2 + lado3; 
 }
@@ -14,6 +31,27 @@ function areaTriangulo(lado, base) {
 // 	btnPeri.addEventListener("click", clickBtnPerimetro)
 		
 // }
+
+//Aqui interactuamos con el HTML
+function calcularPerimetroCuadrado() {
+	const input = document.getElementById("inputCuadrado");
+	const value = input.value;
+  
+	const perimetro = perimetroCuadrado(value);
+	const mostrarPerimetro = document.getElementById('mostrarPerimetro');
+	mostrarPerimetro.innerText = "El perimetro del cuadrado son: " + perimetro;
+	// alert(perimetro);
+  }
+  
+  function calcularAreaCuadrado() {
+	const input = document.getElementById("inputCuadrado");
+	const value = input.value;
+  
+	const area = areaCuadrado(value);
+	const mostrarArea = document.getElementById("mostrarArea");
+	mostrarArea.innerText = "El area del cuadrado son: " + area + "^2";
+	// alert(area);
+  }
 
 function clickBtnPerimetro() {
 	let textLado1 = parseInt(document.getElementById('lado1').value);	
